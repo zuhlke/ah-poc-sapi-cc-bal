b87ef8f2767dc98b80b8029e3415118752ff63ee
romo <robert.moore@zuhlke.com>
2019-05-23 12:31:31 +0100

    place the UniformDistributedRandomNumberGenerator tests into the integration test package

--- src/test/java/com/aimlesshammer/poc/sapi/cc/bal/integration/UniformDistributedRandomNumberGeneratorTest.java
+++ src/test/java/com/aimlesshammer/poc/sapi/cc/bal/integration/UniformDistributedRandomNumberGeneratorTest.java
@@ -1,5 +1,6 @@
-package com.aimlesshammer.poc.sapi.cc.bal;
+package com.aimlesshammer.poc.sapi.cc.bal.integration;
 
+import com.aimlesshammer.poc.sapi.cc.bal.UniformDistributedRandomNumberGenerator;
 import org.junit.Test;
 
 import java.util.HashMap;
