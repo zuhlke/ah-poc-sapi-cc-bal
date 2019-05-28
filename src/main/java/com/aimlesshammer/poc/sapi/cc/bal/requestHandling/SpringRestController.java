eef3495d0ffa0883dcb4ad9757bfa94b7673e8a6
romo <robert.moore@zuhlke.com>
2019-05-28 14:01:24 +0100

    Create domain specific logger fpr the spring rest controller

--- src/main/java/com/aimlesshammer/poc/sapi/cc/bal/requestHandling/SpringRestController.java
+++ src/main/java/com/aimlesshammer/poc/sapi/cc/bal/requestHandling/SpringRestController.java
@@ -1,11 +1,9 @@
 package com.aimlesshammer.poc.sapi.cc.bal.requestHandling;
 
-import com.aimlesshammer.poc.sapi.cc.bal.PocSapiCreditCardBalancesApplication;
 import com.aimlesshammer.poc.sapi.cc.bal.randomNumberGenerator.RandomBehaviourPolicy;
 import com.aimlesshammer.poc.sapi.cc.bal.randomNumberGenerator.UniformDistributedRandomNumberGenerator;
 import com.aimlesshammer.poc.sapi.cc.bal.sapiBehaviourPolicy.BadlyFormattedSapiPolicyHeaderException;
 import com.aimlesshammer.poc.sapi.cc.bal.sapiBehaviourPolicy.BehaviourPolicyHeadersFactory;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,19 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-import static com.aimlesshammer.poc.sapi.cc.bal.sapiBehaviourPolicy.CustomBehaviourPolicyHeaders.X_POLICY_DELAY_RANGE_HEADER_NAME;
-import static com.aimlesshammer.poc.sapi.cc.bal.sapiBehaviourPolicy.CustomBehaviourPolicyHeaders.X_POLICY_FAILURE_RATE_HEADER_NAME;
-
 @RestController
 public class SpringRestController {
-    private static final Logger logger = LoggerFactory.getLogger(SpringRestController.class);
+    private final SpringRestControllerLogger logger = new SpringRestControllerLogger(LoggerFactory.getLogger(SpringRestController.class));
     private final RequestHandler requestHandler = new RequestHandler(new RandomBehaviourPolicy(new UniformDistributedRandomNumberGenerator()));
 
     @GetMapping(path = "/customer/{customer-id}/balance", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> balance(@RequestHeader Map<String, String> headers, @PathVariable("customer-id") String customerId) {
-        logger.info(logMessageSingleArg("Requesting balances for customer"), customerId);
-        logger.info(logMessageSingleArg("Header(" + X_POLICY_FAILURE_RATE_HEADER_NAME + ")"), headers.get(X_POLICY_FAILURE_RATE_HEADER_NAME));
-        logger.info(logMessageSingleArg("Header(" + X_POLICY_DELAY_RANGE_HEADER_NAME + ")"), headers.get(X_POLICY_DELAY_RANGE_HEADER_NAME));
+        logger.logBalance(headers, customerId);
         try {
             return requestHandler.balance(BehaviourPolicyHeadersFactory.behaviourPolicyHeaders(headers));
         } catch (BadlyFormattedSapiPolicyHeaderException e) {
@@ -36,28 +29,20 @@ public class SpringRestController {
 
     @PostMapping("failureRatePc/{rate}")
     public ResponseEntity<String> setFailureRate(@PathVariable("rate") Integer rate) {
-        logger.info(logMessageSingleArg("Setting failure rate"), rate);
+        logger.logSetFailureRate(rate);
         return requestHandler.setFailureRate(rate);
     }
 
     @PostMapping("/perRequestDelayRangeMs")
     public ResponseEntity<String> setPerRequestDelayRange(@RequestParam("min") int min, @RequestParam("max") int max) {
-        logger.info(logMessageSingleArg("Setting min per-request delay"), min);
-        logger.info(logMessageSingleArg("Setting max per-request delay"), max);
+        logger.logSetPerRequestDelayRange(min, max);
         return requestHandler.setPerRequestDelayRange(min, max);
     }
 
     @PostMapping("/reset")
-    public ResponseEntity<String> setPerRequestDelayRange() {
-        logger.info(logMessageNoArgs("Resetting SAPI behaviour policy"));
+    public ResponseEntity<String> resetBehaviourPolicy() {
+        logger.logResetBehaviourPolicy();
         return requestHandler.resetBehaviourPolicy();
     }
 
-    private String logMessageNoArgs(String msg) {
-        return PocSapiCreditCardBalancesApplication.LOG_ID + ": " + msg;
-    }
-
-    private String logMessageSingleArg(String msg) {
-        return PocSapiCreditCardBalancesApplication.LOG_ID + ": " + msg + ": '{}'";
-    }
 }
---
+++ src/main/java/com/aimlesshammer/poc/sapi/cc/bal/requestHandling/SpringRestControllerLogger.java
@@ -0,0 +1,56 @@
+package com.aimlesshammer.poc.sapi.cc.bal.requestHandling;
+
+import com.aimlesshammer.poc.sapi.cc.bal.PocSapiCreditCardBalancesApplication;
+import org.slf4j.Logger;
+
+import java.util.Map;
+
+import static com.aimlesshammer.poc.sapi.cc.bal.sapiBehaviourPolicy.CustomBehaviourPolicyHeaders.X_POLICY_DELAY_RANGE_HEADER_NAME;
+import static com.aimlesshammer.poc.sapi.cc.bal.sapiBehaviourPolicy.CustomBehaviourPolicyHeaders.X_POLICY_FAILURE_RATE_HEADER_NAME;
+
+public class SpringRestControllerLogger {
+    private final Logger logger;
+
+    public SpringRestControllerLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void logBalance(Map<String, String> headers, String customerId) {
+        log("Requesting balances for customer", customerId);
+        logHeader(headers, X_POLICY_FAILURE_RATE_HEADER_NAME);
+        logHeader(headers, X_POLICY_DELAY_RANGE_HEADER_NAME);
+    }
+
+    public void logSetFailureRate(Integer rate) {
+        log("Setting failure rate", rate);
+    }
+
+    public void logSetPerRequestDelayRange(int min, int max) {
+        log("Setting min per-request delay", min);
+        log("Setting max per-request delay", max);
+    }
+
+    public void logResetBehaviourPolicy() {
+        log("Resetting SAPI behaviour policy");
+    }
+
+    private void log(String msg, Object arg) {
+        logger.info(logMessageSingleArg(msg), arg);
+    }
+
+    private void logHeader(Map<String, String> headers, String headerName) {
+        logger.info(logMessageSingleArg("Header(" + headerName + ")"), headers.get(headerName));
+    }
+
+    private void log(String msg) {
+        logger.info(logMessageNoArgs(msg));
+    }
+
+    private String logMessageSingleArg(String msg) {
+        return PocSapiCreditCardBalancesApplication.LOG_ID + ": " + msg + ": '{}'";
+    }
+
+    private String logMessageNoArgs(String msg) {
+        return PocSapiCreditCardBalancesApplication.LOG_ID + ": " + msg;
+    }
+}
