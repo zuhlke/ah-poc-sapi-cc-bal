aa3fd7775d37212e2d134fd18ac4ee3322b1f4f8
romo <robert.moore@zuhlke.com>
2019-06-03 14:34:57 +0100

    Move contract test shell script contents into the travis yml to try to get more debug information out of it.

--- run-contract-tests.sh
+++
@@ -1,8 +0,0 @@
-# Assumption: The fixture has already been deployed to the "development" environment in PCF.
-
-# Clone PAPI repository
-git clone https://github.com/zuhlke/ah-poc-papi-springboot.git
-# Go to the contract tests
-cd ah-poc-papi-springboot/contract-tests
-# Run the tests designated for this SAPI against the deployed dev instance of it
-./bin/ah-poc-sapi-cc-bal
