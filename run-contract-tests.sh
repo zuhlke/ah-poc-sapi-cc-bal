fb744926ce0a1eb81ce0689be2cd869cef9f8dc4
romo <robert.moore@zuhlke.com>
2019-06-03 13:56:21 +0100

    Run the contract tests after the deployment to dev

--- run-contract-tests.sh
+++ run-contract-tests.sh
@@ -1,2 +1,8 @@
 echo "Running contract tests"
+
+# Assumption: the fixture has already been deployed to the "development" environment in PCF.
+# Clone PAPI repository
+# Run PAPI contract tests against dev
+
+echo "Done"
 exit 0
