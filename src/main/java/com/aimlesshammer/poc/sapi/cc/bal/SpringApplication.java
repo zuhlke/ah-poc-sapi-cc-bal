29016f719e1af16ae3cc724bd4f98027095697cf
romo <robert.moore@zuhlke.com>
2019-06-24 08:54:38 +0100

    Add on-demand static import for run(...) and exit(...) spring operations.

--- src/main/java/com/aimlesshammer/poc/sapi/cc/bal/SpringApplication.java
+++ src/main/java/com/aimlesshammer/poc/sapi/cc/bal/SpringApplication.java
@@ -3,16 +3,19 @@ package com.aimlesshammer.poc.sapi.cc.bal;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import static org.springframework.boot.SpringApplication.exit;
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class SpringApplication {
     public static final String LOG_ID = "ah-poc-sapi-cc-bal";
     private static ConfigurableApplicationContext context;
 
     public static void start(String[] args) {
-        context = org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+        context = run(SpringApplication.class, args);
     }
 
     public static void stop(int exitCode) {
-        org.springframework.boot.SpringApplication.exit(context, () -> exitCode);
+        exit(context, () -> exitCode);
     }
 }
